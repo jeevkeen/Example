89665df51ab7c271c36ad71371c8e98f1e85f22e
Александр Смирнов <alekssmirnov610@gmail.com>
2026-09-24 00:54:37 +0300

    Added class triangle

--- ExampleProject/ExampleProject/main.cpp
+++ ExampleProject/ExampleProject/main.cpp
@@ -1,17 +1,106 @@
 #include <iostream>
+#include <stdexcept>
+#include <clocale>
+
+
+class Triangle {
+private:
+    double _width;
+    double _height;
+
+public:
+    Triangle() {
+        _width = 1;
+        _height = 1;
+    }
+
+    Triangle(double width, double height) {
+        if (width > 0 && height > 0) {
+            _width = width;
+            _height = height;
+        }
+        else {
+            throw std::invalid_argument(std::string("Error: param(s) must be >0"));
+        }
+    }
+
+    double getWidth() const noexcept {
+        return _width;
+    }
+
+    double getHeight() const noexcept {
+        return _height;
+    }
+
+    void setWidth(double width) {
+        if (width > 0) {
+            _width = width;
+        }
+        else {
+            throw std::invalid_argument(std::string("Error: width must be >0"));
+        }
+    }
+
+    void setHeight(double height) {
+        if (height > 0) {
+            _height = height;
+        }
+        else {
+            throw std::invalid_argument(std::string("Error: height must be >0"));
+        }
+    }
+
+    double getArea() const noexcept {
+        return (0.5 * _width * _height);
+    }
+};
+
 
 int main() {
-	double a, h, S;
-	
-	std::cout << "Enter triangle width: ";
-	std::cin >> a;
+    
+    setlocale(LC_ALL, "Russian");
 
-	std::cout << "Enter triangle height: ";
-	std::cin >> h;
+    int a, h;
+    Triangle t;
 
-	S = 0.5 * a * h;
+    std::cout << "Enter triangle width: ";
+    std::cin >> a;
+    t.setWidth(a);
 
-	std::cout << "Area of triangle is: " << S << std::endl;
+    std::cout << "Enter triangle height: ";
+    std::cin >> h;
+    t.setHeight(h);
 
-	return 0;
+    std::cout << "\n-----TESTS-----\n";
+
+    try {
+        std::cout << "Width: " << t.getWidth() << "\n";
+        std::cout << "Heigth: " << t.getHeight() << "\n";
+        std::cout << "Area: " << t.getArea() << "\n\n";
+
+        t.setWidth(12);
+        std::cout << "New area after setWidth(12): " << t.getArea() << "\n\n";
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Catch exception: " << e.what() << "\n";
+    }
+
+    try {
+        std::cout << "Try to create triangle with height < 0:\n";
+        Triangle invalidTriangle(8, -3);
+    }
+    catch (const std::invalid_argument& e) {
+        std::cerr << "Catch std::invalid_argument: " << e.what() << "\n\n";
+    }
+
+    try {
+        Triangle t(4, 6);
+        std::cout << "Try to create width = 0:\n";
+        t.setWidth(0);
+    }
+    catch (const std::invalid_argument& e) {
+        std::cerr << "Catch std::invalid_argument: " << e.what() << "\n";
+    }
+
+    return 0;
 }
